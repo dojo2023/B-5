fbda48e75684473f0db50b995fb2cdb789e578c1
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-16 17:13:53 +0900

    Create UsersDAO.java

---
+++ src/src/dao/UsersDAO.java
@@ -0,0 +1,104 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Users;
+
+public class UsersDAO {
+	// ログインできるならtrueを返す
+	public boolean isLoginOK(Users users) {
+		Connection conn = null;
+		boolean loginResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+			// SELECT文を準備する
+			String sql = "select count(*) from users where users_name = ? and users_password = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(2,users.getUsers_name());
+			pStmt.setString(3,users.getUsers_password());
+
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// ユーザーニックネームとパスワードが一致するユーザーがいたかどうかをチェックする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					loginResult = false;
+				}
+			}
+		}
+
+		// 結果を返す
+		return loginResult;
+	}
+
+	public boolean InsertUser(Users users) {
+	    Connection conn = null;
+	    boolean updateSuccess = false;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+			// Prepare the SQL statement
+	        String sql = "INSERT INTO users (users_name, users_password) VALUES (?, ?)";
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	        pstmt.setString(2, users.getUsers_password());
+	        pstmt.setString(3, users.getUsers_name());
+
+	        // Execute the update statement
+	        pstmt.executeUpdate();
+
+	        // The update was successful
+	        updateSuccess = true;
+
+	        pstmt.close();
+
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return updateSuccess;
+	}
+}
+
+
