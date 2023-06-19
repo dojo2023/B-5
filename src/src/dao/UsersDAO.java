586e4c96d3ae2d325a1e86806e8f2fe9d31bfd59
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-19 21:35:30 +0900

    Update UsersDAO.java

--- src/src/dao/UsersDAO.java
+++ src/src/dao/UsersDAO.java
@@ -1,10 +1,10 @@
 package dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import model.Users;
 
@@ -100,6 +100,77 @@ public class UsersDAO {
 
 	    return updateSuccess;
 	}
+
+	public int getUserID(String users_name, String users_password) {
+	    Connection conn = null;
+	    int userID = -1; // Initialize with an invalid value
+
+	    try {
+	    	// JDBCドライバを読み込む
+	    	Class.forName("org.h2.Driver");
+
+	    	// データベースに接続する
+	    	conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+
+	        String sql = "SELECT users_id FROM users WHERE users_name = ? AND users_password = ?";
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	        pstmt.setString(1, users_name);
+	        pstmt.setString(2, users_password);
+
+	        ResultSet rs = pstmt.executeQuery();
+	        if (rs.next()) {
+	            userID = rs.getInt("users_id");
+	        }
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return userID;
+	}
+
+	public int getMaxUserID() {
+	    Connection conn = null;
+	    int maxUserID = -1; // Initialize with an invalid value
+
+	    try {
+	        // JDBCドライバを読み込む
+	        Class.forName("org.h2.Driver");
+
+	        // データベースに接続する
+	        conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+	        String sql = "SELECT MAX(users_id) AS max_id FROM users";
+	        Statement stmt = conn.createStatement();
+
+	        ResultSet rs = stmt.executeQuery(sql);
+	        if (rs.next()) {
+	            maxUserID = rs.getInt("max_id");
+	        }
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return maxUserID;
+	}
+
+
 }
 
 
