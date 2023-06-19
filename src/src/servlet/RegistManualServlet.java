6612244f05ba1a874bcfff05e1281b918046176a
dojo2023-sasakura <130334531+dojo2023-sasakura@users.noreply.github.com>
2023-06-19 16:20:31 +0900

    Create RegistManualServlet.java

---
+++ src/src/servlet/RegistManualServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistManualServlet
+ */
+@WebServlet("/RegistManualServlet")
+public class RegistManualServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/registmanual.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("registmanual").equals("次へ")){
+			request.getAttribute("/sante/RegistPhysicalsServlet");
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/registphysicals.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
