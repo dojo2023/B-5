da2166af095cbc5521f0c7203b3a7c9b358ba443
dojo2023-sasakura <130334531+dojo2023-sasakura@users.noreply.github.com>
2023-06-19 16:20:27 +0900

    Update MypageServlet.java

--- src/src/servlet/MypageServlet.java
+++ src/src/servlet/MypageServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,16 +20,24 @@ public class MypageServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}*/
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		if(request.getParameter("submit").equals("コンディションとアルコール耐性")) {
+			request.getAttribute("/sante/MypagePhysicalsServlet");
+		}else if (request.getParameter("submit").equals("使い方")){
+			request.getAttribute("/sante/MypageManualServlet");
+		}
 	}
 
 }
