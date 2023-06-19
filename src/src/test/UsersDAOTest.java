f19fe90b8b4e43a796d16d36a64aaf1484cec556
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-19 21:35:40 +0900

    Update UsersDAOTest.java

--- src/src/test/UsersDAOTest.java
+++ src/src/test/UsersDAOTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import dao.UsersDAO;
-import model.Users;
 
 public class UsersDAOTest {
     public static void main(String[] args) {
@@ -9,17 +8,14 @@ public class UsersDAOTest {
     }
 
     private static void testIsLoginOK() {
-        UsersDAO usersDAO = new UsersDAO();
-        Users user1 = new Users(1, "yasuo", "yasuo", null, null, null);
-        Users user2 = new Users(0, "nohope", "nohope", null, null, null);
-        Users user3 = new Users(0, "impossible", "impsible", null, null, null);
+		UsersDAO iDao = new UsersDAO();
+		int users_id1 = iDao.getUserID("users_name", "users_password");
+		int users_id2 = iDao.getUserID("yasuo2", "yasuo2");
 
-        boolean result1 = usersDAO.isLoginOK(user1);
-        boolean result2 = usersDAO.isLoginOK(user2);
-        boolean result3 = usersDAO.isLoginOK(user3);
 
-        System.out.println("Login Result for user1: " + result1);
-        System.out.println("Login Result for user2: " + result2);
-        System.out.println("Login Result for user3: " + result3);
+
+
+        System.out.println("Login Result for user1: " + users_id1);
+        System.out.println("Login Result for user2: " + users_id2);
     }
 }
