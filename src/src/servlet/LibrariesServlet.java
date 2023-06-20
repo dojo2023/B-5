091ec0b7833a75b0ef145b731e3de7f1b3737d90
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-20 16:50:15 +0900

    Update LibrariesServlet.java

--- src/src/servlet/LibrariesServlet.java
+++ src/src/servlet/LibrariesServlet.java
@@ -44,23 +44,34 @@ public class LibrariesServlet extends HttpServlet {
 //					response.sendRedirect("/sante/LoginServlet");
 //					return;
 //				}
+//		Map<String, String[]> form = request.getParameterMap();
+//		String dispatcher =
+//				form.containsKey("narrow")?"/NarrowLibrariesServlet":
+//				form.containsKey("post")?"/PostLibrariesServlet":
+//				form.containsKey("bookmarks")?"/BookmarksServlet":
+//					null;
+//		if(dispatcher == null) {
+//
+//		}else {
+//			getServletContext().getRequestDispatcher(dispatcher).forward(request, response);
+//		}
+		request.setCharacterEncoding("UTF-8");
 		if(request.getParameter("submit").equals("検索")) {
+			//ここだけ修正必要
 			//検索結果を表示
 			response.sendRedirect("/sante/LibrariesServlet");
-			return;
+
 		}else if(request.getParameter("submit").equals("絞り込み")){
 			//絞り込み検索画面へとリダイレクトする。
 			response.sendRedirect("/sante/NarrowLibrariesServlet");
-			return;
 		}else if(request.getParameter("submit").equals("投稿")) {
 			//投稿画面へとリダイレクトする。
 			response.sendRedirect("/sante/PostLibrariesServlet");
-			return;
-		}else if(request.getParameter("submit").equals("お気に入り")){
+		}else if(request.getParameter("submit").equals("お気に入り") ){
 			//お気に入り画面へとリダイレクトする。
 			response.sendRedirect("/sante/BookmarksServlet");
-			return;
 		}
+
 	}
 
 }
