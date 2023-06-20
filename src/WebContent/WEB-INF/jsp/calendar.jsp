0de93843bbe9f46a4c9f2f19dcf621823f47d120
dojo2023-sakuraba <130334730+dojo2023-sakuraba@users.noreply.github.com>
2023-06-20 13:03:39 +0900

    櫻庭

---
+++ src/WebContent/WEB-INF/jsp/calendar.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html>
+<html class="html">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- 機能名|santé ! -->
+    <title>カレンダー|santé!</title>
+    <!-- hrefの後ろにcssのリンクを書く -->
+    <link rel="stylesheet" href="calendar.css">
+</head>
+
+<div class="wrapper">
+    <!-- header -->
+    <div class="header">
+        <header>
+            <!-- 各ページへのリンク -->
+            <nav>
+                <ul>
+                    <li><a href="/sante/CalendarServlet">カレンダー</a></li>
+                    <li><a href="/sante/LibrariesServlet">図鑑</a></li>
+                    <li><a href="/sante/CountsServlet">集計</a></li>
+                    <li><a href="/sante/MypageServlet">マイページ</a></li>
+                    <li><a href="/sante/UsersResultServlet">ログアウト</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <!-- body -->
+    <div class="body">
+        <body>
+            <div class="calendar_management">
+                <button id="prev-button" type="button" class="calender_prev">前の月</button>
+                <label id="yearMonth" class="calender_yearmonth"></label>
+                <label class="calender_countdown">次の予定まであと</label>
+                <label class="calender_condition">コンディション</label>
+                <button id="next-button" type="button" class="calender_next">次の月</button>
+                <table>
+                    <thead>
+                        <tr>
+                            <th class="sunday">日</th>
+                            <th>月</th>
+                            <th>火</th>
+                            <th>水</th>
+                            <th>木</th>
+                            <th>金</th>
+                            <th class="saturday">土</th>
+                        </tr>
+                    </thead>
+                    <tbody id="calendar-body" onclick="clickDisplayAlert()">
+                    </tbody>
+            </div>
+            <script src="Calendar.js"></script>
+        </body>
+    </div>
+    <!-- footer -->
+    <div class="footer">
+        <footer>
+
+        </footer>
+    </div>
+</div>
+
+</html>
--- src/WebContent/js/calendar/Counts.js
+++ src/WebContent/js/calendar/Counts.js
@@ -1,2 +1,144 @@
 'use strict';
 
+
+// 現在の年と月を取得
+var currentDate = new Date();
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; // 0から11で表されるため、1を加えて月を表す
+
+var selectedDate = null;
+
+
+
+// カレンダーを生成
+function generateCalendar(year, month) {
+    var calendarBody = document.getElementById('calendar-body');
+    calendarBody.innerHTML = '';
+
+    // 当月の日数を取得
+    var currentMonthLastDate = new Date(year, month, 0).getDate();
+
+    // 当月の最初の曜日を取得
+    var firstDayOfWeek = new Date(year, month - 1, 1).getDay();
+
+    var date = 1;
+    var isCurrentMonth = false;
+
+    for (var i = 0; i < 6; i++) {
+        var row = document.createElement('tr');
+
+        for (var j = 0; j < 7; j++) {
+            var cell = document.createElement('td');
+
+            if (i === 0 && j < firstDayOfWeek) {
+                // 前月の最終日を取得
+                var prevMonthLastDate = new Date(year, month - 1, 0).getDate();
+                // var prevMonthDate = prevMonthLastDate - firstDayOfWeek + j + 1;
+                // cell.textContent = prevMonthDate;
+                // cell.classList.add('prev-month');
+            } else if (date > currentMonthLastDate) {
+                // 翌月の日にちを表示しない
+                cell.classList.add('next-month');
+            } else {
+                cell.textContent = date;
+
+                if (year === currentDate.getFullYear() && month === currentDate.getMonth() + 1 && date === currentDate.getDate()) {
+                    cell.classList.add('current-date');
+                    isCurrentMonth = true;
+                }
+
+                date++;
+            }
+
+            row.appendChild(cell);
+        }
+
+        calendarBody.appendChild(row);
+    }
+
+    // 前月・次月ボタンの状態を制御
+    var prevButton = document.getElementById('prev-button');
+    var nextButton = document.getElementById('next-button');
+
+    if (!isCurrentMonth) {
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+    } else {
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+    }
+
+    // 日付セルのクリックイベントの設定
+    var dateCells = document.querySelectorAll('#calendar-body td');
+    dateCells.forEach(function(cell) {
+        if (!cell.classList.contains('prev-month') && !cell.classList.contains('next-month')) {
+            cell.addEventListener('click', function() {
+                // 選択した日付を更新
+                var selectedDay = parseInt(cell.textContent);
+                selectedDate = new Date(year, month - 1, selectedDay);
+
+                // 選択状態のスタイルを設定
+                dateCells.forEach(function(cell) {
+                    cell.classList.remove('selected-date');
+                });
+                cell.classList.add('selected-date');
+
+                // 選択した日付を表示
+                var selectedDateText = selectedDate.toLocaleDateString('ja-JP', { year: 'numeric', month: 'short', day: 'numeric' });
+                document.getElementById('selected-date').textContent = selectedDateText;
+            });
+        }
+    });
+}
+
+// 前月を表示
+function showPreviousMonth() {
+    if (month === 1) {
+        year--;
+        month = 12;
+    } else {
+        month--;
+    }
+
+    generateCalendar(year, month);
+    document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
+}
+
+// 次月を表示
+function showNextMonth() {
+    if (month === 12) {
+        year++;
+        month = 1;
+    } else {
+        month++;
+    }
+
+    generateCalendar(year, month);
+    document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
+}
+
+// カレンダー生成とボタンのクリックイベントの設定
+document.addEventListener('DOMContentLoaded', function() {
+    generateCalendar(year, month);
+
+    var prevButton = document.getElementById('prev-button');
+    prevButton.addEventListener('click', showPreviousMonth);
+
+    var nextButton = document.getElementById('next-button');
+    nextButton.addEventListener('click', showNextMonth);
+
+});
+
+function clickDisplayAlert() {
+    var selected_year = selectedDate.getFullYear();
+    var selected_month = selectedDate.getMonth() + 1;
+    var selected_day = selectedDate.getDate();
+    alert(selected_year + "年" + selected_month + "月" + selected_day + "日");
+  }
+
+var selected_year = document.getElementById("calenderYear");
+var selected_month = document.getElementById("calenderMonth");
+
+
+//年月を表示
+document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
