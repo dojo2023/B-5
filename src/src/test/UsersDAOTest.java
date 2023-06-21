a301cbe4267f96175ac22cc8ae029747db9e5359
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-21 22:00:35 +0900

    Update UsersDAOTest.java

--- src/src/test/UsersDAOTest.java
+++ src/src/test/UsersDAOTest.java
@@ -4,18 +4,8 @@ import dao.UsersDAO;
 
 public class UsersDAOTest {
     public static void main(String[] args) {
-        testIsLoginOK();
-    }
-
-    private static void testIsLoginOK() {
-		UsersDAO iDao = new UsersDAO();
-		int users_id1 = iDao.getUserID("users_name", "users_password");
-		int users_id2 = iDao.getUserID("yasuo2", "yasuo2");
-
-
-
-
-        System.out.println("Login Result for user1: " + users_id1);
-        System.out.println("Login Result for user2: " + users_id2);
+        UsersDAO usersDAO = new UsersDAO();
+        int maxUserID = usersDAO.getMaxUserID();
+        System.out.println("Max UserID: " + maxUserID);
     }
 }
