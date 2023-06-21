d7632e518216b54247b624e80fbd79ec005d235a
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 15:46:29 +0900

    Update LibrariesServlet.java

--- src/src/servlet/LibrariesServlet.java
+++ src/src/servlet/LibrariesServlet.java
@@ -19,13 +19,14 @@ public class LibrariesServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
-//				HttpSession session = request.getSession();
-//				if (session.getAttribute("id") == null) {
-//					response.sendRedirect("/sante/LoginServlet");
-//					return;
-//				}
+		//				HttpSession session = request.getSession();
+		//				if (session.getAttribute("id") == null) {
+		//					response.sendRedirect("/sante/LoginServlet");
+		//					return;
+		//				}
 
 		//検索処理を行う
 
@@ -37,37 +38,27 @@ public class LibrariesServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
-//				HttpSession session = request.getSession();
-//				if (session.getAttribute("id") == null) {
-//					response.sendRedirect("/sante/LoginServlet");
-//					return;
-//				}
-//		Map<String, String[]> form = request.getParameterMap();
-//		String dispatcher =
-//				form.containsKey("narrow")?"/NarrowLibrariesServlet":
-//				form.containsKey("post")?"/PostLibrariesServlet":
-//				form.containsKey("bookmarks")?"/BookmarksServlet":
-//					null;
-//		if(dispatcher == null) {
-//
-//		}else {
-//			getServletContext().getRequestDispatcher(dispatcher).forward(request, response);
-//		}
+		//				HttpSession session = request.getSession();
+		//				if (session.getAttribute("id") == null) {
+		//					response.sendRedirect("/sante/LoginServlet");
+		//					return;
+		//				}
 		request.setCharacterEncoding("UTF-8");
-		if(request.getParameter("submit").equals("検索")) {
+		if (request.getParameter("submit").equals("検索")) {
 			//ここだけ修正必要
 			//検索結果を表示
 			response.sendRedirect("/sante/LibrariesServlet");
 
-		}else if(request.getParameter("submit").equals("絞り込み")){
+		} else if (request.getParameter("submit").equals("絞り込み")) {
 			//絞り込み検索画面へとリダイレクトする。
 			response.sendRedirect("/sante/NarrowLibrariesServlet");
-		}else if(request.getParameter("submit").equals("投稿")) {
+		} else if (request.getParameter("submit").equals("投稿")) {
 			//投稿画面へとリダイレクトする。
 			response.sendRedirect("/sante/PostLibrariesServlet");
-		}else if(request.getParameter("submit").equals("お気に入り") ){
+		} else if (request.getParameter("submit").equals("お気に入り")) {
 			//お気に入り画面へとリダイレクトする。
 			response.sendRedirect("/sante/BookmarksServlet");
 		}
