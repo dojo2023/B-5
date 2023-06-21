c452b4266d3477b6002725de1788db0abc18e0c3
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 15:46:27 +0900

    Update BookmarksServlet.java

--- src/src/servlet/BookmarksServlet.java
+++ src/src/servlet/BookmarksServlet.java
@@ -19,26 +19,42 @@ public class BookmarksServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
-				//		HttpSession session = request.getSession();
-				//		if (session.getAttribute("id") == null) {
-				//			response.sendRedirect("/sante/LoginServlet");
-				//			return;
-				//		}
+		//		HttpSession session = request.getSession();
+		//		if (session.getAttribute("id") == null) {
+		//			response.sendRedirect("/sante/LoginServlet");
+		//			return;
+		//		}
 
-				//検索処理を行う
+		//検索処理を行う
 
-				//librariesページにフォワードする。
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bookmarks.jsp");
-				dispatcher.forward(request, response);
+		//bookmarksページにフォワードする。
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bookmarks.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
+		//		HttpSession session = request.getSession();
+		//		if (session.getAttribute("id") == null) {
+		//			response.sendRedirect("/sante/LoginServlet");
+		//			return;
+		//		}
+		request.setCharacterEncoding("UTF-8");
+		if(request.getParameter("submit").equals("更新")) {
+			// 更新内容をセッションスコープに入れる必要あり。要記載
+			response.sendRedirect("/sante/BookmarksConfirmServlet");
 
+		}else if(request.getParameter("submit").equals("キャンセル")){
+			//図鑑表示画面へとリダイレクトする。
+			response.sendRedirect("/sante/LibrariesServlet");
+		}
 	}
 
 }
