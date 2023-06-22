89ad74a04b0ea801efa916f11133cf10be0827b4
dojo2023-sakuraba <130334730+dojo2023-sakuraba@users.noreply.github.com>
2023-06-22 16:47:18 +0900

    Update Schedules.js

--- src/WebContent/js/schedules/Schedules.js
+++ src/WebContent/js/schedules/Schedules.js
@@ -1,145 +1,115 @@
 'use strict';
 
-// 現在の年と月を取得
-var currentDate = new Date();
-var year = currentDate.getFullYear();
-var month = currentDate.getMonth() + 1; // 0から11で表されるため、1を加えて月を表す
+const weeks = ['日', '月', '火', '水', '木', '金', '土']
+const date = new Date()
+let year = date.getFullYear()
+let month = date.getMonth() + 1
+const config = {
+    show: 1,
+}
 
-var selectedDate = null;
+function showCalendar(year, month) {
+    for (i = 0; i < config.show; i++) {
+        const calendarHtml = createCalendar(year, month)
+        const sec = document.createElement('section')
+        sec.innerHTML = calendarHtml
+        document.querySelector('#calendar').appendChild(sec)
 
+        month++
+        if (month > 12) {
+            year++
+            month = 1
+        }
+    }
+}
 
+function createCalendar(year, month) {
+    const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+    const endDate = new Date(year, month, 0) // 月の最後の日を取得
+    const endDayCount = endDate.getDate() // 月の末日
+    const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
+    const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+    const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+    let dayCount = 1 // 日にちのカウント
+    let calendarHtml = '' // HTMLを組み立てる変数
 
-// カレンダーを生成
-function generateCalendar(year, month) {
-    var calendarBody = document.getElementById('calendar-body');
-    calendarBody.innerHTML = '';
+    calendarHtml += '<h1>' + year + '/' + month + '</h1>'
+    calendarHtml += '<table>'
 
-    // 当月の日数を取得
-    var currentMonthLastDate = new Date(year, month, 0).getDate();
+    // 曜日の行を作成
+    for (let i = 0; i < weeks.length; i++) {
+        calendarHtml += '<td>' + weeks[i] + '</td>'
+    }
 
-    // 当月の最初の曜日を取得
-    var firstDayOfWeek = new Date(year, month - 1, 1).getDay();
+    for (let w = 0; w < 6; w++) {
+        calendarHtml += '<tr>'
 
-    var date = 1;
-    var isCurrentMonth = false;
-
-    for (var i = 0; i < 6; i++) {
-        var row = document.createElement('tr');
-
-        for (var j = 0; j < 7; j++) {
-            var cell = document.createElement('td');
-
-            if (i === 0 && j < firstDayOfWeek) {
-                // 前月の最終日を取得
-                var prevMonthLastDate = new Date(year, month - 1, 0).getDate();
-                // var prevMonthDate = prevMonthLastDate - firstDayOfWeek + j + 1;
-                // cell.textContent = prevMonthDate;
-                // cell.classList.add('prev-month');
-            } else if (date > currentMonthLastDate) {
-                // 翌月の日にちを表示しない
-                cell.classList.add('next-month');
+        for (let d = 0; d < 7; d++) {
+            if (w == 0 && d < startDay) {
+                // 1行目で1日の曜日の前
+                let num = lastMonthendDayCount - startDay + d + 1
+                calendarHtml += '<td class="is-disabled">' + num + '</td>'
+            } else if (dayCount > endDayCount) {
+                // 末尾の日数を超えた
+                let num = dayCount - endDayCount
+                calendarHtml += '<td class="is-disabled">' + num + '</td>'
+                dayCount++
             } else {
-                cell.textContent = date;
-
-                if (year === currentDate.getFullYear() && month === currentDate.getMonth() + 1 && date === currentDate.getDate()) {
-                    cell.classList.add('current-date');
-                    isCurrentMonth = true;
-                }
-
-                date++;
+                calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
+                dayCount++
             }
-
-            row.appendChild(cell);
         }
-
-        calendarBody.appendChild(row);
+        calendarHtml += '</tr>'
     }
+    calendarHtml += '</table>'
 
-    // 前月・次月ボタンの状態を制御
-    var prevButton = document.getElementById('prev-button');
-    var nextButton = document.getElementById('next-button');
+    return calendarHtml
+}
 
-    if (!isCurrentMonth) {
-        prevButton.disabled = false;
-        nextButton.disabled = false;
-    } else {
-        prevButton.disabled = false;
-        nextButton.disabled = false;
-    }
+function moveCalendar(e) {
+    document.querySelector('#calendar').innerHTML = ''
 
-    // 日付セルのクリックイベントの設定
-    var dateCells = document.querySelectorAll('#calendar-body td');
-    dateCells.forEach(function(cell) {
-        if (!cell.classList.contains('prev-month') && !cell.classList.contains('next-month')) {
-            cell.addEventListener('click', function() {
-                // 選択した日付を更新
-                var selectedDay = parseInt(cell.textContent);
-                selectedDate = new Date(year, month - 1, selectedDay);
+    if (e.target.id === 'prev') {
+        month--
 
-                // 選択状態のスタイルを設定
-                dateCells.forEach(function(cell) {
-                    cell.classList.remove('selected-date');
-                });
-                cell.classList.add('selected-date');
-
-                // 選択した日付を表示
-                var selectedDateText = selectedDate.toLocaleDateString('ja-JP', { year: 'numeric', month: 'short', day: 'numeric' });
-                document.getElementById('selected-date').textContent = selectedDateText;
-            });
+        if (month < 1) {
+            year--
+            month = 12
         }
-    });
-}
-
-// 前月を表示
-function showPreviousMonth() {
-    if (month === 1) {
-        year--;
-        month = 12;
-    } else {
-        month--;
     }
 
-    generateCalendar(year, month);
-    document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
-}
+    if (e.target.id === 'next') {
+        month++
 
-// 次月を表示
-function showNextMonth() {
-    if (month === 12) {
-        year++;
-        month = 1;
-    } else {
-        month++;
+        if (month > 12) {
+            year++
+            month = 1
+        }
     }
 
-    generateCalendar(year, month);
-    document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
+    showCalendar(year, month)
 }
 
-// カレンダー生成とボタンのクリックイベントの設定
-document.addEventListener('DOMContentLoaded', function() {
-    generateCalendar(year, month);
+document.querySelector('#prev').addEventListener('click', moveCalendar)
+document.querySelector('#next').addEventListener('click', moveCalendar)
 
-    var prevButton = document.getElementById('prev-button');
-    prevButton.addEventListener('click', showPreviousMonth);
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("calendar_td")) {
+        // dataを日付型に型変換し年、月、日で分解し格納
+        var OnclickDate = new Date(e.target.dataset.date);
+        var selected_year = OnclickDate.getFullYear();
+        var selected_month = OnclickDate.getMonth() + 1;
+        var selected_day = OnclickDate.getDate();
+        // 過去か未来かで分岐処理
+        if (OnclickDate > date) {
+            alert( "未来" + selected_year + "年" + selected_month + "月" + selected_day + "日");
+        }else{
+            alert( "過去" + selected_year + "年" + selected_month + "月" + selected_day + "日");
+        }
+    }
+})
 
-    var nextButton = document.getElementById('next-button');
-    nextButton.addEventListener('click', showNextMonth);
-
-});
-
-function clickDisplayAlert() {
-    var selected_year = selectedDate.getFullYear();
-    var selected_month = selectedDate.getMonth() + 1;
-    var selected_day = selectedDate.getDate();
-    alert(selected_year + "年" + selected_month + "月" + selected_day + "日");
-  }
-
-//年月を表示
-var selected_year = document.getElementById("calenderYear");
-var selected_month = document.getElementById("calenderMonth");
-
-document.querySelector('#yearMonth').innerHTML = year + "年 " + month + "月";
+showCalendar(year, month)
 /**
  *
  */
