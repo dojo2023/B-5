e401df92c5b996d14193eb087a7110c6b8b47704
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-23 10:31:52 +0900

    Update PostLibrariesConfirmServlet.java

--- src/src/servlet/PostLibrariesConfirmServlet.java
+++ src/src/servlet/PostLibrariesConfirmServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.LibrariesDAO;
+import model.PostLibraries;
 /**
  * Servlet implementation class PostLibrariesConfirmServlet
  */
@@ -47,9 +49,30 @@ public class PostLibrariesConfirmServlet extends HttpServlet {
 				//		}
 		if(request.getParameter("submit").equals("はい")) {
 			// DBにinsertする
-
-			// セッションスコープの破棄
 			HttpSession session = request.getSession();
+			PostLibraries post_lib = (PostLibraries) session.getAttribute("post_lib");
+
+//			int libraries_id = post_lib.getLibraries_id;
+			String post_description = post_lib.getPost_description();
+			String post_genre = post_lib.getPost_genre();
+			String post_kind = post_lib.getPost_kind();
+			String post_alcon = post_lib.getPost_alcon();
+			String post_from = post_lib.getPost_from();
+			String post_remarks = post_lib.getPost_remarks();
+
+			System.out.println(post_description);
+			System.out.println(post_genre);
+			System.out.println(post_kind);
+			System.out.println(post_alcon);
+			System.out.println(post_from);
+			System.out.println(post_remarks);
+
+
+			LibrariesDAO librariesDAO = new LibrariesDAO();
+			boolean insertSuccess = librariesDAO.insertLibraries(new PostLibraries(post_description, post_genre, post_kind, post_alcon, post_from, post_remarks));
+
+			if (insertSuccess) {
+			// セッションスコープの破棄
 	        session.removeAttribute("post_lib");
 	        response.sendRedirect("/sante/LibrariesServlet");
 		}else if(request.getParameter("submit").equals("いいえ")) {
@@ -57,4 +80,5 @@ public class PostLibrariesConfirmServlet extends HttpServlet {
 		}
 	}
 
+}
 }
