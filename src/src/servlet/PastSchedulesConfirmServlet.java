5fbef3da8df788bdba77aecd5d78b9ee743a7170
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-23 18:51:13 +0900

    Create PastSchedulesConfirmServlet.java

---
+++ src/src/servlet/PastSchedulesConfirmServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PastSchedulesComfirmServlet
+ */
+@WebServlet("/PastSchedulesConfirmServlet")
+public class PastSchedulesConfirmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
+		//				HttpSession session = request.getSession();
+		//				if (session.getAttribute("id") == null) {
+		//					response.sendRedirect("/sante/LoginServlet");
+		//					return;
+		//				}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/pastschedulesconfirm.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
