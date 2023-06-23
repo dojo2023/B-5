1640dd60412248a94ac289e67708c706728c62af
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-23 18:51:07 +0900

    Update CalenderServlet.java

--- src/src/servlet/CalenderServlet.java
+++ src/src/servlet/CalenderServlet.java
@@ -65,11 +65,26 @@ public class CalenderServlet extends HttpServlet {
 			System.out.println("格納:" + add_schedules.getEditing_schedules_users_id() + "\n");
 			// 画面を遷移
 			response.sendRedirect("/sante/AddSchedulesServlet");
-
 		}else if(request.getParameter("submit").equals("過去予定有")) {
 
+			// 画面を遷移
+			response.sendRedirect("/sante/PastSchedulesConfirmServlet");
 		}else if(request.getParameter("submit").equals("未来予定無")) {
-
+			// ダミーの値を生成
+						String editing_schedules_name = "ダミー未来予定無";
+						Date dummy = new Date();
+						String editing_schedules_dt = new SimpleDateFormat("yyyy/MM/dd").format(dummy);
+						int editing_schedules_users_id = 1;
+						// ダミーの追加情報を格納
+						EditingSchedules add_schedules = new EditingSchedules(editing_schedules_name, editing_schedules_dt,
+								editing_schedules_users_id);
+						// セッションスコープに追加情報を保存
+						session.setAttribute("add_schedules", add_schedules);
+						System.out.println("格納:" + add_schedules.getEditing_schedules_name() + "\n");
+						System.out.println("格納:" + add_schedules.getEditing_schedules_dt() + "\n");
+						System.out.println("格納:" + add_schedules.getEditing_schedules_users_id() + "\n");
+						// 画面を遷移
+						response.sendRedirect("/sante/AddSchedulesServlet");
 		}else if(request.getParameter("submit").equals("未来予定有")) {
 
 		}
