47f63d7978e3f0f881b2588ba1c01651648f8f19
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-26 15:16:04 +0900

    Update CountsServlet.java

--- src/src/servlet/CountsServlet.java
+++ src/src/servlet/CountsServlet.java
@@ -23,78 +23,73 @@ public class CountsServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/sante/LoginServlet");
-//			return;
-//		}
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}
 
 		// 集計入力画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
 		dispatcher.forward(request, response);
-		
 
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-			HttpSession session = request.getSession();
-	//		if (session.getAttribute("id") == null) {
-	//			response.sendRedirect("/sante/LoginServlet");
-	//			return;
-	//			}
-				
-		
-			if (request.getParameter("submit").equals("次へ")) {
-				try {
-					int all_member = Integer.parseInt(request.getParameter("counts"));
-					
-					if (all_member != 0) {
-						PostCounts counts_cou = new PostCounts();
-						// 投稿情報を格納
-						counts_cou.setAll_member(all_member);
-						session.setAttribute("post_counts",counts_cou );
-	
-						
-						if (all_member < 0) {
-							// 集計入力画面にフォワードする
-							RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
-							dispatcher.forward(request, response);
-						}
-						else if (all_member > 10) {
-							// 集計入力画面にフォワードする
-							RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
-							dispatcher.forward(request, response);
-						}
-					}
-					else if (all_member == 0) {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
+		HttpSession session = request.getSession();
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}
+
+		if (request.getParameter("submit").equals("次へ")) {
+			try {
+				int all_member = Integer.parseInt(request.getParameter("counts"));
+
+				if (all_member != 0) {
+					PostCounts counts_cou = new PostCounts();
+					// 投稿情報を格納
+					counts_cou.setAll_member(all_member);
+					session.setAttribute("post_counts", counts_cou);
+
+					if (all_member < 0) {
+						// 集計入力画面にフォワードする
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
+						dispatcher.forward(request, response);
+					} else if (all_member > 10) {
 						// 集計入力画面にフォワードする
 						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
 						dispatcher.forward(request, response);
 					}
-					
-					
-				}
-				catch(NumberFormatException n) {
+				} else if (all_member == 0) {
 					// 集計入力画面にフォワードする
 					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
 					dispatcher.forward(request, response);
 				}
-				
 
-				finally {
-					//CountSortingServletサーブレットに処理を渡す(リダイレクト)
-					response.sendRedirect("/sante/CountsSortingServlet");
-				}
-				
-				
+			} catch (NumberFormatException n) {
+				// 集計入力画面にフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/counts.jsp");
+				dispatcher.forward(request, response);
 			}
+
+			finally {
+				//CountSortingServletサーブレットに処理を渡す(リダイレクト)
+				response.sendRedirect("/sante/CountsSortingServlet");
+			}
+
+		}
 	}
 
 }
