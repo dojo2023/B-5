8b53cf41ca78d7d0e6baf8603dc6fd01c0a56d34
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-26 15:15:48 +0900

    Update CountsCupsServlet.java

--- src/src/servlet/CountsCupsServlet.java
+++ src/src/servlet/CountsCupsServlet.java
@@ -23,67 +23,63 @@ public class CountsCupsServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
-/*		if (session.getAttribute("id") == null) {
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
 			response.sendRedirect("/sante/LoginServlet");
 			return;
 		}
-*/
-		
-		
+
 		// 杯数選択画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/countscups.jsp");
 		dispatcher.forward(request, response);
-		
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/sante/LoginServlet");
-//			return;
-//		}
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}
 
-			
-			request.setCharacterEncoding("UTF-8");
-			
-			ArrayList<String> counts_list = new ArrayList<String>();
-			if (request.getParameter("submit").equals("登録")) {
-				
-				for (int i = 1; i < 11; i++) {
-					if (request.getParameter("counts_cups" + i) != null) {
-						System.out.println(request.getParameter("counts_cups" + i));
-						System.out.println("counts_cups" + i);
+		request.setCharacterEncoding("UTF-8");
+
+		ArrayList<String> counts_list = new ArrayList<String>();
+		if (request.getParameter("submit").equals("登録")) {
+
+			for (int i = 1; i < 11; i++) {
+				if (request.getParameter("counts_cups" + i) != null) {
+					System.out.println(request.getParameter("counts_cups" + i));
+					System.out.println("counts_cups" + i);
 
 					String counts_cups = request.getParameter("counts_cups" + i);
-				
+
 					counts_list.add(counts_cups);
-					}
 				}
-				
-				session.setAttribute("counts_list",counts_list );
-					
-				System.out.println(counts_list);
-
-
-					
-				//集計登録確認画面のサーブレットにリダイレクト
-				response.sendRedirect("/sante/CountsRegistConfirmServlet");
-				
-				
-					
 			}
-			else if (request.getParameter("submit").equals("割合表示")) {
-				//割合画面のサーブレットにリダイレクト
-				response.sendRedirect("/sante/CountsRatioServlet");
-			}
-		
+
+			session.setAttribute("counts_list", counts_list);
+
+			System.out.println(counts_list);
+
+			//集計登録確認画面のサーブレットにリダイレクト
+			response.sendRedirect("/sante/CountsRegistConfirmServlet");
+
+		} else if (request.getParameter("submit").equals("割合表示")) {
+			//割合画面のサーブレットにリダイレクト
+			response.sendRedirect("/sante/CountsRatioServlet");
+		}
+
 	}
 
 }
