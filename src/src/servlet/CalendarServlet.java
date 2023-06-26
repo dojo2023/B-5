b0537033196b534c71fdf0b7ca261365c5579f10
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-26 15:15:38 +0900

    Update CalendarServlet.java

--- src/src/servlet/CalendarServlet.java
+++ src/src/servlet/CalendarServlet.java
@@ -27,11 +27,12 @@ public class CalendarServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
-		//				HttpSession session = request.getSession();
-		//				if (session.getAttribute("id") == null) {
-		//					response.sendRedirect("/sante/LoginServlet");
-		//					return;
-		//				}
+		HttpSession session = request.getSession();
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendar.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -44,12 +45,13 @@ public class CalendarServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
-		//		if (session.getAttribute("id") == null) {
-		//			response.sendRedirect("/sante/LoginServlet");
-		//			return;
-		//		}
+		if (session.getAttribute("users_id") == null) {
+			System.out.println("ログイン失敗");
+			response.sendRedirect("/sante/LoginServlet");
+			return;
+		}
 		// ダミーボタンの処理なので最終的に全部消すこと。
-		if(request.getParameter("submit").equals("過去予定無")) {
+		if (request.getParameter("submit").equals("過去予定無")) {
 			// ダミーの値を生成
 			String editing_schedules_name = "ダミー過去予定無";
 			Date dummy = new Date();
@@ -65,27 +67,27 @@ public class CalendarServlet extends HttpServlet {
 			System.out.println("格納:" + add_schedules.getEditing_schedules_users_id() + "\n");
 			// 画面を遷移
 			response.sendRedirect("/sante/AddSchedulesServlet");
-		}else if(request.getParameter("submit").equals("過去予定有")) {
+		} else if (request.getParameter("submit").equals("過去予定有")) {
 
 			// 画面を遷移
 			response.sendRedirect("/sante/PastSchedulesConfirmServlet");
-		}else if(request.getParameter("submit").equals("未来予定無")) {
+		} else if (request.getParameter("submit").equals("未来予定無")) {
 			// ダミーの値を生成
-						String editing_schedules_name = "ダミー未来予定無";
-						Date dummy = new Date();
-						String editing_schedules_dt = new SimpleDateFormat("yyyy/MM/dd").format(dummy);
-						int editing_schedules_users_id = 1;
-						// ダミーの追加情報を格納
-						EditingSchedules add_schedules = new EditingSchedules(editing_schedules_name, editing_schedules_dt,
-								editing_schedules_users_id);
-						// セッションスコープに追加情報を保存
-						session.setAttribute("add_schedules", add_schedules);
-						System.out.println("格納:" + add_schedules.getEditing_schedules_name() + "\n");
-						System.out.println("格納:" + add_schedules.getEditing_schedules_dt() + "\n");
-						System.out.println("格納:" + add_schedules.getEditing_schedules_users_id() + "\n");
-						// 画面を遷移
-						response.sendRedirect("/sante/AddSchedulesServlet");
-		}else if(request.getParameter("submit").equals("未来予定有")) {
+			String editing_schedules_name = "ダミー未来予定無";
+			Date dummy = new Date();
+			String editing_schedules_dt = new SimpleDateFormat("yyyy/MM/dd").format(dummy);
+			int editing_schedules_users_id = 1;
+			// ダミーの追加情報を格納
+			EditingSchedules add_schedules = new EditingSchedules(editing_schedules_name, editing_schedules_dt,
+					editing_schedules_users_id);
+			// セッションスコープに追加情報を保存
+			session.setAttribute("add_schedules", add_schedules);
+			System.out.println("格納:" + add_schedules.getEditing_schedules_name() + "\n");
+			System.out.println("格納:" + add_schedules.getEditing_schedules_dt() + "\n");
+			System.out.println("格納:" + add_schedules.getEditing_schedules_users_id() + "\n");
+			// 画面を遷移
+			response.sendRedirect("/sante/AddSchedulesServlet");
+		} else if (request.getParameter("submit").equals("未来予定有")) {
 
 		}
 	}
