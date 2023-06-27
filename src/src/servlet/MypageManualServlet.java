ee26eb5cb33d315131c7dca1046fe25d288032af
dojo2023-sasakura <130334531+dojo2023-sasakura@users.noreply.github.com>
2023-06-27 17:29:10 +0900

    Update MypageManualServlet.java

--- src/src/servlet/MypageManualServlet.java
+++ src/src/servlet/MypageManualServlet.java
@@ -23,12 +23,12 @@ public class MypageManualServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
-		HttpSession session = request.getSession();
+		/*HttpSession session = request.getSession();
 		if (session.getAttribute("users_id") == null) {
 			System.out.println("ログイン失敗");
 			response.sendRedirect("/sante/LoginServlet");
 			return;
-		}
+		}*/
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypagemanual.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -49,4 +49,5 @@ public class MypageManualServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 
 	}
-}
+	}
+
