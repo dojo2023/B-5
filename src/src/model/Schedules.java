e926b45bb0115610c7e4c1734aa2597a4a599390
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-27 09:25:01 +0900

    Update Schedules.java

--- src/src/model/Schedules.java
+++ src/src/model/Schedules.java
@@ -6,6 +6,8 @@ public class Schedules {
 	private int schedules_id;
 	private String schedules_name;
 	private Timestamp schedules_dt;
+	private String schedules_date;
+	private String schedules_time;
 	private int users_id;
 	private Timestamp created_at;
 	private Timestamp updated_at;
@@ -20,6 +22,16 @@ public class Schedules {
 		this.users_id = users_id;
 		this.created_at = created_at;
 		this.updated_at = updated_at;
+	}
+
+	public Schedules(int schedules_id, String schedules_name, Timestamp schedules_dt, String schedules_date, String schedules_time, int users_id) {
+		super();
+		this.schedules_id = schedules_id;
+		this.schedules_name = schedules_name;
+		this.schedules_dt = schedules_dt;
+		this.schedules_date = schedules_date;
+		this.schedules_time = schedules_time;
+		this.users_id = users_id;
 	}
 
 	public Schedules() {
@@ -57,6 +69,22 @@ public class Schedules {
 		this.schedules_dt = schedules_dt;
 	}
 
+	public String getSchedules_date() {
+		return schedules_date;
+	}
+
+	public void setSchedules_date(String schedules_date) {
+		this.schedules_date = schedules_date;
+	}
+
+	public String getSchedules_time() {
+		return schedules_time;
+	}
+
+	public void setSchedules_time(String schedules_time) {
+		this.schedules_time = schedules_time;
+	}
+
 	public int getUsers_id() {
 		return users_id;
 	}
@@ -80,5 +108,6 @@ public class Schedules {
 	public void setUpdated_at(Timestamp updated_at) {
 		this.updated_at = updated_at;
 	}
+
 
 }
