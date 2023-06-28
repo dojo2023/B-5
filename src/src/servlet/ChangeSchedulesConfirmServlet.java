860937c1ad4b1a68f2b66f82d58a070bb4fc9f0a
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-29 01:27:14 +0900

    Update ChangeSchedulesConfirmServlet.java

--- src/src/servlet/ChangeSchedulesConfirmServlet.java
+++ src/src/servlet/ChangeSchedulesConfirmServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import dao.SchedulesDAO;
 
 /**
  * Servlet implementation class ChangeSchedulesConfirmServlet
@@ -20,6 +23,7 @@ public class ChangeSchedulesConfirmServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
 		if (session.getAttribute("users_id") == null) {
@@ -35,6 +39,7 @@ public class ChangeSchedulesConfirmServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする。
 		HttpSession session = request.getSession();
 		if (session.getAttribute("users_id") == null) {
@@ -42,8 +47,27 @@ public class ChangeSchedulesConfirmServlet extends HttpServlet {
 			response.sendRedirect("/sante/LoginServlet");
 			return;
 		}
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		if ("更新".equals(request.getParameter("submit"))) {
+			// IDとってきたはずなんで(Timestamp型なのでもしかしたら年月日合ってても時間違うとエラー吐くかも。)
+			// update文のところ作ってもらってもいいですか？構造分からなかったです。
+
+			String delete_schedules_name = (String) session.getAttribute("schedules_name");
+			String delete_schedules_date = (String) session.getAttribute("schedules_date");
+			Timestamp delete_ts = Timestamp.valueOf(delete_schedules_date);
+			SchedulesDAO sDAO = new SchedulesDAO();
+			sDAO.getSchedulesId(delete_schedules_name, delete_ts);
+
+			// 画面を遷移
+			session.removeAttribute("schedules_date");
+			session.removeAttribute("schedules_name");
+			response.sendRedirect("/sante/CarendarServlet");
+			return;
+		} else if ("キャンセル".equals(request.getParameter("submit"))) {
+
+			// 画面を遷移
+			response.sendRedirect("/sante/ChangeSchedulesConfirmServlet");
+			return;
+		}
 	}
 
 }
