e12b4d2f7ee54724d6a49727c6c4abb71233303c
dojo2023-fujio <fujiohirohikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-28 17:28:01 +0900

    Update Schedules.js

--- src/WebContent/js/schedules/Schedules.js
+++ src/WebContent/js/schedules/Schedules.js
@@ -5,65 +5,65 @@ const date = new Date()
 let year = date.getFullYear()
 let month = date.getMonth() + 1
 const config = {
-    show: 1,
+	show: 1,
 }
 
 function showCalendar(year, month) {
-    for (let i = 0; i < config.show; i++) {
-        const calendarHtml = createCalendar(year, month)
-        const sec = document.createElement('section')
-        sec.innerHTML = calendarHtml
-        document.querySelector('#calendar').appendChild(sec)
+	for (let i = 0; i < config.show; i++) {
+		const calendarHtml = createCalendar(year, month)
+		const sec = document.createElement('section')
+		sec.innerHTML = calendarHtml
+		document.querySelector('#calendar').appendChild(sec)
 
-        month++
-        if (month > 12) {
-            year++
-            month = 1
-        }
-    }
+		month++
+		if (month > 12) {
+			year++
+			month = 1
+		}
+	}
 }
 
 function createCalendar(year, month) {
-    const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
-    const endDate = new Date(year, month, 0) // 月の最後の日を取得
-    const endDayCount = endDate.getDate() // 月の末日
-    const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
-    const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
-    const startDay = startDate.getDay() // 月の最初の日の曜日を取得
-    let dayCount = 1 // 日にちのカウント
-    let calendarHtml = '' // HTMLを組み立てる変数
+	const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+	const endDate = new Date(year, month, 0) // 月の最後の日を取得
+	const endDayCount = endDate.getDate() // 月の末日
+	const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
+	const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+	const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+	let dayCount = 1 // 日にちのカウント
+	let calendarHtml = '' // HTMLを組み立てる変数
 
-    calendarHtml += '<h1>' + year + '/' + month + '</h1>'
-    calendarHtml += '<table>'
+	calendarHtml += '<h1>' + year + '/' + month + '</h1>'
+	calendarHtml += '<table>'
 
-    // 曜日の行を作成
-    for (let i = 0; i < weeks.length; i++) {
-        calendarHtml += '<td>' + weeks[i] + '</td>'
-    }
+	// 曜日の行を作成
+	for (let i = 0; i < weeks.length; i++) {
+		calendarHtml += '<td>' + weeks[i] + '</td>'
+	}
 
-    for (let w = 0; w < 6; w++) {
-        calendarHtml += '<tr>'
+	for (let w = 0; w < 6; w++) {
+		calendarHtml += '<tr>'
 
-        for (let d = 0; d < 7; d++) {
-            if (w == 0 && d < startDay) {
-                // 1行目で1日の曜日の前
-                let num = lastMonthendDayCount - startDay + d + 1
-                calendarHtml += '<td class="is-disabled">' + num + '</td>'
-            } else if (dayCount > endDayCount) {
-                // 末尾の日数を超えた
-                let num = dayCount - endDayCount
-                calendarHtml += '<td class="is-disabled">' + num + '</td>'
-                dayCount++
-            } else {
-                calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
-                dayCount++
-            }
-        }
-        calendarHtml += '</tr>'
-    }
-    calendarHtml += '</table>'
+		for (let d = 0; d < 7; d++) {
+			if (w == 0 && d < startDay) {
+				// 1行目で1日の曜日の前
+				let num = lastMonthendDayCount - startDay + d + 1
+				calendarHtml += '<td class="is-disabled">' + num + '</td>'
+			} else if (dayCount > endDayCount) {
+				// 末尾の日数を超えた
+				let num = dayCount - endDayCount
+				calendarHtml += '<td class="is-disabled">' + num + '</td>'
+				dayCount++
+			} else {
+				calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
+				dayCount++
+			}
+		}
+		calendarHtml += '</tr>'
+	}
+	calendarHtml += '</table>'
 
-    return calendarHtml
+	return calendarHtml
 }
 
 //表示されているカレンダーの年月を取得
@@ -75,29 +75,29 @@ let display_month_pre;
 let display_month = month;
 
 function moveCalendar(e) {
-    document.querySelector('#calendar').innerHTML = ''
+	document.querySelector('#calendar').innerHTML = ''
 
-    if (e.target.id === 'prev') {
-        month--
+	if (e.target.id === 'prev') {
+		month--
 
-        if (month < 1) {
-            year--
-            month = 12
-        }
-    }
+		if (month < 1) {
+			year--
+			month = 12
+		}
+	}
 
-    if (e.target.id === 'next') {
-        month++
+	if (e.target.id === 'next') {
+		month++
 
-        if (month > 12) {
-            year++
-            month = 1
-        }
-    }
+		if (month > 12) {
+			year++
+			month = 1
+		}
+	}
 
-    showCalendar(year, month)
+	showCalendar(year, month)
 
-    year_month = document.getElementById("calendar");
+	year_month = document.getElementById("calendar");
 
 	display_year_month = year_month.querySelector("h1").textContent;
 	//console.log("year_day:" + display_year_month);
@@ -122,14 +122,11 @@ document.addEventListener("click", function (e) {
         // dataを日付型に型変換し年、月、日で分解し格納
         var OnclickDate = new Date(e.target.dataset.date);
         var selected_year = OnclickDate.getFullYear();
-        var selected_month = OnclickDate.getMonth() + 1;
-        var selected_day = OnclickDate.getDate();
-        // 過去か未来かで分岐処理
-        if (OnclickDate > date) {
-            alert( "未来" + selected_year + "年" + selected_month + "月" + selected_day + "日");
-        }else{
-            alert( "過去" + selected_year + "年" + selected_month + "月" + selected_day + "日");
-        }
+         var selected_month = (OnclickDate.getMonth() + 1).toString().padStart(2, '0');
+        var selected_day = OnclickDate.getDate().toString().padStart(2, '0');
+
+        var date_input = document.getElementsByName("select_day")[0];
+        date_input.value = selected_year + "-" + selected_month + "-" + selected_day;
     }
 })
 
@@ -162,17 +159,17 @@ function schedules_insert() {
 		const date_year = date.substring(0, 4)
 		const date_month = date.substring(5, 7)
 		const date_day = date.substring(8)
-		console.log("id:"+id);
-		console.log("dt:"+date);
-		console.log("date_year:"+date_year);
-		console.log("date_month:"+date_month);
-		console.log("date_day:"+date_day);
+		console.log("id:" + id);
+		console.log("dt:" + date);
+		console.log("date_year:" + date_year);
+		console.log("date_month:" + date_month);
+		console.log("date_day:" + date_day);
 
 		//カレンダーにセッションスコープの日時のデータが有ればその日のtdに追加
 		for (let i = 1; i <= calender_td.length; i++) {
 			//年月日で一緒かどうかを判別
 			if (display_year == date_year && display_month == date_month && i == date_day) {
-				calender_td[i-1].innerText = i + "\n" + name;
+				calender_td[i - 1].innerText = i + "\n" + name;
 				//console.log(calender_td[i].textContent);
 			}
 
