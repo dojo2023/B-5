0f024fbb1b4d052aa181948d2c1d96828459712f
dojo2023-chuan <130334647+dojo2023-chuan@users.noreply.github.com>
2023-06-28 09:21:44 +0900

    Update SchedulesDAO.java

--- src/src/dao/SchedulesDAO.java
+++ src/src/dao/SchedulesDAO.java
@@ -107,5 +107,128 @@ public class SchedulesDAO {
 	    return schedulesList;
 	}
 
+	// schedulesを削除
+	public boolean deleteSchedules(int schedules_id) {
+	    Connection conn = null;
+	    boolean deleteSuccess = false;
+
+	    try {
+	        // JDBCドライバを読み込む
+	        Class.forName("org.h2.Driver");
+
+	        // データベースに接続する
+	        conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+	        // Prepare the SQL statement
+	        String sql = "DELETE FROM schedules WHERE schedules_id = ?";
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	        pstmt.setInt(1, schedules_id);
+
+	        // Execute the update statement
+	        int rowsAffected = pstmt.executeUpdate();
+
+	        if (rowsAffected > 0) {
+	            // The delete was successful
+	            deleteSuccess = true;
+	        }
+
+	        pstmt.close();
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return deleteSuccess;
+	}
+
+	// schedulesを更新
+	public boolean updateSchedules(Schedules schedules) {
+	    Connection conn = null;
+	    boolean updateSuccess = false;
+
+	    try {
+	        // JDBCドライバを読み込む
+	        Class.forName("org.h2.Driver");
+
+	        // データベースに接続する
+	        conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+	        // Prepare the SQL statement
+	        String sql = "UPDATE schedules SET schedules_name = ?, schedules_dt = ? WHERE schedules_id = ?";
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	        pstmt.setString(1, schedules.getSchedules_name());
+	        pstmt.setTimestamp(2, schedules.getSchedules_dt());
+	        pstmt.setInt(3, schedules.getSchedules_id());
+
+	        // Execute the update statement
+	        int rowsAffected = pstmt.executeUpdate();
+
+	        if (rowsAffected > 0) {
+	            // The update was successful
+	            updateSuccess = true;
+	        }
+
+	        pstmt.close();
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return updateSuccess;
+	}
+
+	// スケジュールのIDを取得
+	public int getSchedulesId(String name, Timestamp dt) {
+	    Connection conn = null;
+	    int schedulesId = -1;
+
+	    try {
+	        // JDBCドライバを読み込む
+	        Class.forName("org.h2.Driver");
+
+	        // データベースに接続する
+	        conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/B5", "yasuo", "yasuo");
+
+	        String sql = "SELECT schedules_id FROM schedules WHERE schedules_name = ? AND schedules_dt = ?";
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	        pstmt.setString(1, name);
+	        pstmt.setTimestamp(2, dt);
+
+	        ResultSet rs = pstmt.executeQuery();
+	        if (rs.next()) {
+	            schedulesId = rs.getInt("schedules_id");
+	        }
+
+	        pstmt.close();
+	        rs.close();
+	    } catch (SQLException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return schedulesId;
+	}
+
 
 }
